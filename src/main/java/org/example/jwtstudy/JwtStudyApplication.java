7a54797d884e8b4737762fe320e85425035d1c9b
se0ngeun <se0ngeun@dgsw.hs.kr>
2024-07-24 20:56:14 +0900

    :heavy_plus_sign: Add JWT dependency

---
+++ src/main/java/org/example/jwtstudy/JwtStudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.jwtstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtStudyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/jwtstudy/controller/MainController.java
@@ -0,0 +1,15 @@
+package org.example.jwtstudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody // 웹이 아닌 객체나 문자열을 반환
+public class MainController {
+
+    @GetMapping("/")
+    public String mainP() {
+        return "Main Controller";
+    }
+}
