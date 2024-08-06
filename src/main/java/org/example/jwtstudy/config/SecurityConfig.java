afaf577dd3df758579fea41a2598ad734ddb13d0
se0ngeun <se0ngeun@dgsw.hs.kr>
2024-08-06 14:49:04 +0900

    Add Spring Security configuration

--- src/main/java/org/example/jwtstudy/config/SecurityConfig.java
+++ src/main/java/org/example/jwtstudy/config/SecurityConfig.java
@@ -1,12 +1,10 @@
 package org.example.jwtstudy.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
