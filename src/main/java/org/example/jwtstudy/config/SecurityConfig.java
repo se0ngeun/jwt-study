f09842bed1c492179494602a4ac70efc5e517751
se0ngeun <se0ngeun@dgsw.hs.kr>
2024-08-06 14:47:04 +0900

    Add Spring Security configuration

---
+++ src/main/java/org/example/jwtstudy/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package org.example.jwtstudy.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // crsf disable
+        http
+                .csrf((auth) -> auth.disable());
+
+        // Form 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        // http basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        // 경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "join").permitAll() // 모든 권한 허용
+                        .requestMatchers("/admin").hasRole("ADMIN") // admin 권한을 가진 사용자만 접근 가능
+                        .anyRequest().authenticated()); // login한 사용자만 접근 가능
+
+        // 세션 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        return http.build();
+    }
+}
